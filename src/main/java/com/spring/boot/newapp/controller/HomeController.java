cd4479b5d53416babb3e3b889bae3d3e74678b52
melaniebalam <melanie.pucbalam@cbtis72.edu.mx>
2022-11-02 17:59:33 -0500

    Terminando de subir la seguridad

--- src/main/java/com/spring/boot/newapp/controller/HomeController.java
+++ src/main/java/com/spring/boot/newapp/controller/HomeController.java
@@ -3,13 +3,25 @@ package com.spring.boot.newapp.controller;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,6 +29,7 @@ import com.spring.boot.newapp.model.Compras;
 import com.spring.boot.newapp.model.Menu;
 import com.spring.boot.newapp.model.Perfil;
 import com.spring.boot.newapp.model.Usuario;
+import com.spring.boot.newapp.service.ICategoriasService;
 import com.spring.boot.newapp.service.IComprasService;
 import com.spring.boot.newapp.service.IMenuService;
 import com.spring.boot.newapp.service.IUsuariosService;
@@ -24,11 +37,14 @@ import com.spring.boot.newapp.service.IUsuariosService;
 
 @Controller
 public class HomeController {
-	
+	@Autowired
+	private ICategoriasService serviceCategorias;
 	@Autowired
     private IUsuariosService serviceUsuarios;
 	@Autowired
 	private IMenuService serviceMenu;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/tabla")
 	public String mostrarTabla(Model model) {
@@ -64,12 +80,7 @@ public class HomeController {
 		public String mostrarabout(Model model) {
 			return "sobre";
 		} 
-		//Encargado de renderizar la pagina principal
-		@GetMapping("/book") 
-		public String mostrarbook(Model model) {
-			return "book";
-
-		}
+	
 		
 		@GetMapping("/cartmenu") 
 		public String mostrarmenu(Model model) {
@@ -80,29 +91,57 @@ public class HomeController {
 		} 
 		
 		
-		/*@GetMapping("/sobre") 
-		public String mostrarsobre(Model model) {
-			List<Menu> lista = serviceMenu.buscarTodo();
-			model.addAttribute("menus", lista);
-			return "sobre";
-
-		} */
-		
-
 		@GetMapping("/descuentos") 
 		public String mostrardesc(Model model) {
 			List<Menu> lista = serviceMenu.buscarTodo();
 			model.addAttribute("menus", lista);
 			return "descuentos";
 		} 
+		@GetMapping("/login" )
+		public String mostrarLogin() {
+		return "formLogin";
+		}
+		@GetMapping("/logout")
+		public String logout(HttpServletRequest request){
+		SecurityContextLogoutHandler logoutHandler =
+		new SecurityContextLogoutHandler();
+		logoutHandler.logout(request, null, null);
+		return "redirect:/login";
+		}
+		@GetMapping("/bcrypt/{texto}")
+		@ResponseBody // sirve para que se renderize el texto y no una vista con el nombre
+		public String encriptar(@PathVariable("texto") String texto) {
+			return texto + " Encriptado en bcrypt: "+ passwordEncoder.encode(texto);
+		}
 		
+		@GetMapping("/index")
+		public String mostrarIndex(Authentication auth, HttpSession session) {
+			String username = auth.getName();
+			System.out.println("Nombre del usuario: " + username);
+			
+			for (GrantedAuthority rol: auth.getAuthorities()) { // es para que muestre que rol tiene
+				System.out.println("ROL: "+ rol.getAuthority());
+			}
+			if (session.getAttribute("usuario") == null) {
+				Usuario usuario = serviceUsuarios.buscarPorUsername(username);
+				usuario.setPassword(null); // para que no guarde la contraseña o no lo muestre
+				System.out.println("Usuario: " + usuario);
+				// para buscar datos en la sesion
+				session.setAttribute("usuario", usuario);
+			}
+			
+			return "redirect:/";
+		}
 		@GetMapping("/signup")
 		public String registrarse(Usuario usuario) {
-			return "usuarios/formRegistro";
+			return "usuarios/formRegistro"; 
 		}
 		//Ejercicio
 		@PostMapping("/signup")
 		public String guardarRegistro(Usuario usuario, BindingResult result, RedirectAttributes attributes) {
+			 String pwdPlano = usuario.getPassword();
+			 String pwdEncriptado = passwordEncoder.encode(pwdPlano);
+			 usuario.setPassword(pwdEncriptado);
 			//Ejercicio.
 			 usuario.setEstatus(1);
 			 usuario.setFechaRegistro(new Date());
@@ -130,7 +169,11 @@ public class HomeController {
 		public void setGenericos(Model model) {
 			model.addAttribute("menus", serviceMenu.buscarOfertas());
 		}
-		
+		// FIN DE LOS USUARIOS
+		@InitBinder
+		public void initBinder(WebDataBinder binder) {
+			binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+		}
 		
 		
 
--- src/main/java/com/spring/boot/newapp/security/DatabaseWebSecurity.java
+++ src/main/java/com/spring/boot/newapp/security/DatabaseWebSecurity.java
@@ -3,11 +3,14 @@ package com.spring.boot.newapp.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -27,48 +30,26 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 
 	}
 
-	/*@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-		// Los recursos estáticos no requieren autenticación
-		.antMatchers(
-		"/bootstrap/**", 
-		"/images/**",
-		"/tinymce/**",
-		"/logos/**").permitAll()
-
-		// Las vistas públicas no requieren autenticación
-		.antMatchers("/", 
-		"/signup",
-		"/menu/view/**").permitAll()
-		// Asignar permisos a URLs por ROLES
-		.antMatchers("/categorias/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR") 
-		.antMatchers("/menu/indexPaginate/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
-		.antMatchers("/menu").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR", "USUARIO")
-		.antMatchers("/usuarios/**").hasAnyAuthority("ADMINISTRADOR")
-		.antMatchers("/sobre").hasAnyAuthority("USUARIO","SUPERVISOR","ADMINISTRADOR")
-
-		// Todas las demás URLs de la Aplicación requieren autenticación
-		.anyRequest().authenticated()
-		// El formulario de Login no requiere autenticacion
-		.and().formLogin().permitAll();
-	}*/
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 	// Los recursos estáticos no requieren autenticación
-				.antMatchers("/bootstrap/**", "/images/**", "/tinymce/**", "/logos/**").permitAll()
+				.antMatchers("/bootstrap/**","/css/**","/js/**", "/images/**", "/tinymce/**", "/logos/**").permitAll()
 	// Las vistas públicas no requieren autenticación
-				.antMatchers("/", "/signup", "/menu/view/**").permitAll()
+				.antMatchers("/", "/signup", "/bcrypt/**", "/menu/view/**").permitAll()
 	// Asignar permisos a URLs por ROLES PARA QUE QUE DEPENDIENDO DEL ROL QUE TENGAS ES COMO VAS ACCEDER A LAS SECCIONES
 				.antMatchers("/menu/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
 				.antMatchers("/categorias/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
 				.antMatchers("/usuarios/**").hasAnyAuthority("ADMINISTRADOR")
-				.antMatchers("/acerca").hasAnyAuthority("USUARIO", "SUPERVISOR","ADMINISTRADOR")
+				.antMatchers("/acerca").hasAnyAuthority("USUARIO","SUPERVISOR","ADMINISTRADOR")
 	// Todas las demás URLs de la Aplicación requieren autenticación
 				.anyRequest().authenticated()
 	// El formulario de Login no requiere autenticacion
-				.and().formLogin().permitAll();
+				.and().formLogin().loginPage("/login").permitAll();
 	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	return new BCryptPasswordEncoder();
+}
+	
 }
